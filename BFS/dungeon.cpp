2aa0a046234525ecd83d5d2c4156a1697d2cf320
Eric Brown <ewic98@gmail.com>
2022-10-16 20:53:05 +0300

    nodes woring

--- BFS/dungeon.cpp
+++ BFS/dungeon.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 struct node {
-    node* parent;
+    node* parent = NULL;
     pair<int, int> data;
 };
 
@@ -33,23 +33,29 @@ int dc[MOVE_DIRECTIONS] = { 0, 0, 1, -1 };
 
 
 
-int solve();
-vector<node> explore_neighbors(int r, int c);
-void reconstruct_path(pair<int, int> s, pair<int, int> e, vector<node> prev);
+int solve(int er, int ec);
+vector<node*> explore_neighbors(int r, int c, vector<node*> prev);
+vector<pair<int, int>> reconstruct_path(pair<int, int> e, vector<node*> prev);
+//node find_node(pair<int, int> rc, vector<node> prev);
+int find_node(pair<int, int> coord, vector<node*> prev);
+void draw_map();
+void add_coord_to_matrix(pair<int, int> p, char c);
+void add_coord_to_matrix(int pr, int pc, char c);
 
 int main() {
 
     cout << "In BFS dungeon main" << endl;
 
-    // init where the Exit spot it=s
-    m[3][3] = 'E';
+    // init where the Exit spot is
+    int er = 3, ec = 3;
+    m[er][ec] = 'E';
 
     // TODO: init walls
     // walls/rocks will be '#'
 
     // pass prev by reference
 
-    int moves = solve();
+    int moves = solve(er, ec);
 
     if (moves >= 0) {
         cout << "It took " << moves << " moves to leave the dungeon." << endl;
@@ -61,7 +67,7 @@ int main() {
     return 1;
 }
 
-int solve() {
+int solve(int er, int ec) {
     // add star coordinates to the queues
     rq.push(sr);
     cq.push(sc);
@@ -69,10 +75,11 @@ int solve() {
     visited[sr][sc] = true;
 
     // INIT NODE LIST
-    node head;
-    head.data = pair<int, int>(sr, sc);
+    node* head = new node;
+    head->data = pair<int, int>(sr, sc);
+    head->parent = NULL;
     // tracks parent node to recreate path (R,C)
-    vector<node> prev = {head};
+    vector<node*> prev = {head};
 
     while(rq.size() > 0 || cq.size() > 0) {
         int r = rq.front(); rq.pop();
@@ -80,10 +87,11 @@ int solve() {
 
         if(m[r][c] == 'E') {
             reached_end = true;
+            cout << "actual E: " << r << ", " << c << endl;
             break;
         }
 
-        prev.push_back(explore_neighbors(r, c, prev.back()));
+        prev = explore_neighbors(r, c, prev);
         nodes_left_in_layer--;
 
         if(nodes_left_in_layer == 0) {
@@ -94,14 +102,24 @@ int solve() {
     }
 
     if(reached_end) {
+
+        cout << "E: " << prev.back()->data.first << ", " <<  prev.back()->data.second << endl;
+        // RECONSTRUCT PATH
+        vector<pair<int, int>> path = reconstruct_path(pair<int, int>(er, ec), prev);
+        // ADD PATH TO MATRIX m
+
+
+
         return move_count;
     }
 
     return -1;
 }
 
-vector<node> explore_neighbors(int r, int c, node parent) {
-    vector<node> ret;
+vector<node*> explore_neighbors(int r, int c, vector<node*> prev) {
+
+    // do a math thing here to figure out parent node
+    node* parent = prev.at(prev.size() - nodes_left_in_layer);
 
     for(int i = 0; i < MOVE_DIRECTIONS; i++) {
         // get new R & C coordinates for each potential 4 directions
@@ -119,23 +137,58 @@ vector<node> explore_neighbors(int r, int c, node parent) {
         rq.push(rr);
         cq.push(cc);
 
-        node child;
+        // Create child node
+        node* child = new node;
+        child->data = pair<int, int>(rr, cc);
+        child->parent = parent;
+
+        prev.push_back(child);
 
         visited[rr][cc] = true;
         nodes_in_next_layer++;
     }
 
-    if(ret.size() == 0) {
-        return null;
-    }
-    else return ret;
+    return prev;
 }
 
-void reconstruct_path(pair<int, int> s, pair<int, int> e, vector<node> prev) {
+vector<pair<int, int>> reconstruct_path(pair<int, int> e, vector<node*> prev) {
     // Reconstruct path going backwards from E
     vector<pair<int, int>> path;
 
-    for(pair<int, int> at = e; at != null; at = prev[at]) {
+    int index = find_node(e, prev);
+    node* curr = prev.at(index);
 
+
+
+
+    while (curr->parent != NULL) {
+        path.push_back(curr->data);
+        curr = curr->parent;
+        cout << "curr node: " << curr->data.first << ", " << curr->data.second << endl;
     }
+
+    return path;
+}
+
+int find_node(pair<int, int> coord, vector<node*> prev) {
+    cout << "find node" <<endl;
+    for(int i = prev.size() -1; prev.at(i)->parent != NULL && i > 0; --i) {
+        if( prev.at(i)->data.first == coord.first && prev.at(i)->data.second == coord.second) {
+            cout << "found node: " << prev.at(i)->data.first << ", " << prev.at(i)->data.second << " at: " << i <<endl;
+            return i;
+        }
+    }
+
+    cout << "find node done" <<endl;
+
+    return -1;
+}
+
+void add_coord_to_matrix(pair<int, int> p, char c) {
+    // replace the char in matrix m with given char at position p
+    add_coord_to_matrix(p.first, p.second, c);
+}
+
+void add_coord_to_matrix(int pr, int pc, char c) {
+    m[pr][pc] = c;
 }
