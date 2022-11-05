316301621284a985cd561b113bc87aaded4e1681
Eric Brown <ewic98@gmail.com>
2022-11-06 01:39:25 +0200

    changed graph

--- Dijkstra/Eager/dijkstraEager.cpp
+++ Dijkstra/Eager/dijkstraEager.cpp
@@ -1,5 +1,4 @@
 // main file to start Dijkstras Class
-// figure out makefile for incuding class & IndexedPriorityQueue
 
 #include "DijkstrasShortestPathDHeap.h"
 #include "../../Utilities/graphviz.h"
