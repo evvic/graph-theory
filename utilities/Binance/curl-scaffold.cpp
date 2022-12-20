c0289874703081c03054f0a4b446afb9b9dd9801
evvic <ewic98@gmail.com>
2022-12-21 00:25:53 +0700

    init parent class for curl requests

---
+++ utilities/Binance/curl-scaffold.cpp
@@ -0,0 +1,18 @@
+#ifndef CURL_SCAFFOLD
+#define CURL_SCAFFOLD
+
+#include <string>
+
+// Think about renaming file & class
+
+class CurlScaffold {
+private:
+    std::string api_key;
+    std::string secret_key;
+
+public:
+    CurlScaffold();
+
+};
+
+#endif
